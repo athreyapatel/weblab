7e06067fcc1b9b91ea5714a6801024e8c2ec7f12
Athreyapatel <athreyapatel@gmail.com>
2019-11-27 07:28:24 +0530

    commit

---
+++ 7.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <meta http-equiv="refresh" content="1" />
+</head>
+<style>
+    .p {
+        color: blue;
+    }
+</style>
+<body>
+    <h1>Display current date and time</h1>
+    <p> Time is:<?php echo date(" h: i : s A");?> </p>
+    <p> Date is:<?php echo date("d-m-y");?> </p>
+</body>
+
+</html>
